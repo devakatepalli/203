4f753e722c33757ee7c3bc6eb7a4993a21bb6a88
devakatepalli <saikumarkatepalli89@gmail.com>
2025-03-31 13:19:15 +0530

    Added todo app with tests

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,42 +1,34 @@
-class TodoList {
+const fs = require("fs");
+
+class Todo {
   constructor() {
     this.todos = [];
   }
 
-  // Add a new todo
   add(todo) {
-    this.todos.push(todo);
+    this.todos.push({ ...todo, completed: false });
   }
 
-  // Mark a todo as complete
   markAsComplete(index) {
-    if (index >= 0 && index < this.todos.length) {
+    if (this.todos[index]) {
       this.todos[index].completed = true;
     }
   }
 
-  // Get all todos
-  getAll() {
-    return this.todos;
-  }
-
-  // Get overdue todos
-  getOverdueItems() {
+  overdue() {
     const today = new Date().toISOString().split("T")[0];
     return this.todos.filter((todo) => todo.dueDate < today);
   }
 
-  // Get due today todos
-  getDueTodayItems() {
+  dueToday() {
     const today = new Date().toISOString().split("T")[0];
     return this.todos.filter((todo) => todo.dueDate === today);
   }
 
-  // Get due later todos
-  getDueLaterItems() {
+  dueLater() {
     const today = new Date().toISOString().split("T")[0];
     return this.todos.filter((todo) => todo.dueDate > today);
   }
 }
 
-module.exports = TodoList;
+module.exports = Todo;
